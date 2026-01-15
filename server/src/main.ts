5a9b810e901acc38674d19dabbc8cebf8a0ea84e
SiddheshWonderBiz <siddhesh.wayal@wonderbiz.in>
2026-01-15 16:12:02 +0530

    main.ts fixed

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,25 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://appointment-system-kevi3msuy-siddheshwonderbizs-projects.vercel.app/',
-    ],
-    credentials: true,
-  });
 
   app.use(cookieParser());
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      'http://localhost:5173',
+      'https://appointment-system-kevi3msuy-siddheshwonderbizs-projects.vercel.app',
+    ],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
-  console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
   await app.listen(process.env.PORT || 3000);
 }

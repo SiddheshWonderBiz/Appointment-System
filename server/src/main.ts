242e377b33895fa035522d7c0b072e6b4d3adb6c
SiddheshWonderBiz <siddhesh.wayal@wonderbiz.in>
2026-01-15 12:24:33 +0530

    making frontend production ready

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,14 +5,21 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://your-frontend-domain.vercel.app',
+    ],
+    credentials: true,
+  });
+
   app.use(cookieParser());
 
   app.enableCors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
   });
-  console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
+  console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
   await app.listen(process.env.PORT || 3000);
 }
